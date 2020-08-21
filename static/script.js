13d314bb33df8de18ab729668f871952c28b0467
Anshuman Gupta <anshuman844@gmail.com>
2020-08-21 21:55:28 +0530

    Fix undefined var error

--- static/script.js
+++ static/script.js
@@ -1,21 +1,25 @@
 function notifyMe(message, orderid) {
-  console.log('Hi');
+  var 
   if (!("Notification" in window)) {
     alert("This browser does not support desktop notification");
   }
   else if (Notification.permission === "granted") {
     var notification = new Notification(message);
+    notification.onclick = function(event) {
+      event.preventDefault();
+      window.open(`/order/${orderid}`)
+    }
   }
   else if (Notification.permission !== "denied") {
     Notification.requestPermission().then(function (permission) {
       if (permission === "granted") {
         var notification = new Notification(message);
+        notification.onclick = function(event) {
+          event.preventDefault();
+          window.open(`/order/${orderid}`)
+        }
       }
     });
-  }
-  notification.onclick = function(event) {
-    event.preventDefault();
-    window.open(`/order/${orderid}`)
   }
 }
 
